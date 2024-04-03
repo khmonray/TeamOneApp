184b5c25f8915157cbc405566941d14b762b515a
Ainur Khusnullin <monray@rambler.ru>
2024-04-03 12:27:33 +0500

    first commit

---
+++ src/main/java/one/team/TeamOneApp/TeamOneAppApplication.java
@@ -0,0 +1,13 @@
+package one.team.TeamOneApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/team/TeamOneApp/TeamOneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package one.team.TeamOneApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamOneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
