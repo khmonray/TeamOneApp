e4756480542d451f51055815e9bc51ab26e01a91
Ainur Khusnullin <monray@rambler.ru>
2024-04-03 19:55:43 +0500

    divide onto modules

---
+++ business-logic/src/main/java/one/team/BusinessServerApp.java
@@ -0,0 +1,12 @@
+package one.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessServerApp.class, args);
+    }
+}
---
+++ payments/src/main/java/one/team/PaymentsServerApp.java
@@ -0,0 +1,11 @@
+package one.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PaymentsServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsServerApp.class, args);
+    }
+}
